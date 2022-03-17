5c390bb2d7b5420b46255d9d1f32b82d68769bc9
with-rainbow <rainbow-w@qq.com>
2022-03-17 11:33:36 +0800

    提取插件

--- docs/.vuepress/plugins.ts
+++ docs/.vuepress/plugins.ts
@@ -1,10 +1,9 @@
 import { PluginConfig } from "vuepress";
-import path from 'path'
+import path from "path";
 
 let pluginConfig: PluginConfig[] = [
   ["@vuepress/back-to-top"],
-  [ path.resolve(__dirname, './plugins/vuepress-plugin-enhance') ],
-  // [ '@vuepress/last-updated']
-]
+  ["@with-rainbow/enhance"],
+];
 
 export default pluginConfig;
