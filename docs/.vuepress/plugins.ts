372d70a40649a5cbf0c97cb4fbe2c6237fef2011
with-rainbow <rainbow-w@qq.com>
2022-03-17 14:13:35 +0800

    add search plugin

--- docs/.vuepress/plugins.ts
+++ docs/.vuepress/plugins.ts
@@ -4,6 +4,7 @@ import path from "path";
 let pluginConfig: PluginConfig[] = [
   ["@vuepress/back-to-top"],
   ["@with-rainbow/vuepress-plugin-enhance"],
+  ['@vuepress/plugin-search']
 ];
 
 export default pluginConfig;
